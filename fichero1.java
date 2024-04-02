42d13aa6e2f86ce3b38a404606dfd31ffd3384b6
LUCASLOPEZZGZ <lopez.galuc21@zaragoza.salesianos.edu>
2024-04-02 22:12:55 +0200

    archivos creados y modificados

---
+++ fichero1.java
@@ -0,0 +1,5 @@
+public class fichero1{
+public static void main(String[] args){
+    System.out.println("Hola, estas leyendo el fichero 1");
+}
+}
---
+++ fichero2.java
@@ -0,0 +1,5 @@
+public class fichero2 {
+    public static void main(String[] args){
+        System.out.println("Hola, estas leyendo el fichero 2");
+    }
+}
